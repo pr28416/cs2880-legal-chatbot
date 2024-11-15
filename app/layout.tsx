40ebb56c378f2e80d160c35dfba6b6f5e3d7cf03
Pranav Ramesh <pranav.ramesh1@gmail.com>
2024-11-14 22:39:04 -0500

    Fixed build errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,5 @@
 import type { Metadata } from "next";
-import {
-  Poppins,
-  Kanit,
-  Lora,
-  Figtree,
-  Urbanist,
-  Inter_Tight,
-  Raleway,
-  Inter,
-  Playfair_Display,
-  Anek_Devanagari,
-  Merriweather,
-  Roboto_Slab,
-  PT_Sans,
-  Work_Sans,
-  Fira_Sans,
-  Fira_Code,
-  Manrope,
-  Mulish,
-  DM_Sans,
-  IBM_Plex_Mono,
-  Inconsolata,
-  Source_Code_Pro,
-  Noto_Sans_Mono,
-  JetBrains_Mono,
-  Reddit_Mono,
-  Martian_Mono,
-  Red_Hat_Mono,
-  Roboto_Mono,
-  M_PLUS_Code_Latin,
-  Sono,
-  Fira_Mono,
-  Fira_Sans_Condensed,
-  Merriweather_Sans,
-} from "next/font/google";
-import localFont from "next/font/local";
+import { Lora } from "next/font/google";
 import "./globals.css";
 
 const fontFamily = Lora({ subsets: ["latin"] });
